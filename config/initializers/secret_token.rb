c9e07eb8122bf6b3f7a845250ab3724d50cbf77e
Kouhei Sutou <kou@clear-code.com>
2012-12-04 22:03:13 +0900

    rails new code-log

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeLog::Application.config.secret_token = 'fad0cc1290cbf4ba67afc06fa46728bbe9f6ac1a67fa603d0552083fb6b00f47b9b722211605b77903f80cd4cfa0e7b3f264e423415af3fde16e2d85d1fd5ee3'
